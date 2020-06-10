127e5661e51fc84f67974c620035a138795a7eba
FWSU <yys9905@naver.com>
2020-06-10 12:34:59 +0900

    병합 및 설명 수정

--- DVDPriceCalculate.java
+++ DVDPriceCalculate.java
@@ -1,6 +1,6 @@
 
 /**
- * 여기에 DVDPriceCalculate 클래스 설명을 작성하십시오.
+ * DVD요금을 계산하는 프로그램
  * 
  * @author (2018315036 양유석,2018315003 최세원, 2018315022 이지연) 
  * @version (20.06.10)
@@ -8,12 +8,12 @@
 public class DVDPriceCalculate
 {
     /**
-     * 예제 메소드 - DVD요금을 할인하는 메소드
+     * offThePrice - DVD요금을 할인하는 메소드
      * 
-     * @param  price/ age/ old   가격/ 나이/ 구작
-     * @return     18세 이하 price*0.1 / 65세 이상 price*0.2 / 구작 price*0.5 
+     * @param      price, age, old
+     * @return     18세 이하 price*0.9 / 65세 이상 price*0.8 / 구작 price*0.5 
      */
-    public double OffThePrice(double price, int age, int old)
+    public double offThePrice(double price, int age, int old)
     {
         boolean oldDVD = false;
         if (old >= 6){
