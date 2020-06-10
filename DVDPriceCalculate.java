80a5fa88bcd2d1578a63172fc5b3a20c68589630
FWSU <yys9905@naver.com>
2020-06-10 12:09:11 +0900

    add method

---
+++ DVDPriceCalculate.java
@@ -0,0 +1,21 @@
+
+/**
+ * 여기에 DVDPriceCalculate 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class DVDPriceCalculate
+{
+    /**
+     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
+     * 
+     * @param  y   메소드의 예제 매개변수
+     * @return     x 더하기 y 
+     */
+    public int OffThePrice(int price, int age, int old)
+    {
+        
+        return price;
+    }
+}
